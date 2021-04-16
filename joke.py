482f9ecc29d4a74249e110f6bb1773993c06f4e8
Shellywell123 <benjamin@shellswell.com>
2021-04-16 18:39:33 +0100

    joke fix

--- joke.py
+++ joke.py
@@ -5,10 +5,10 @@ import time
 output=(subprocess.getoutput("curl https://official-joke-api.appspot.com/random_joke"))
 
 setup = ((output.split('setup":')[1]).split('","punchline"')[0])[1:]
-punchline = ((output.split('"punchline":')[1]))[1:-1]
+punchline = ((output.split('"punchline":')[1]))[1:-2]
 
 print('\n AI-PI: "',setup,'"')
-os.system('espeak "{}"'.format(setup)
+os.system('espeak "{}"'.format(setup))
 time.sleep(5)
 
 print('\n AI-PI: "',punchline,'"')
