defa8474a720b5f1c66ad088fc68bdf678ce9743
Shellywell123 <benjamin@shellswell.com>
2021-04-16 18:36:59 +0100

    added joke

---
+++ joke.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import time
+
+output=(subprocess.getoutput("curl https://official-joke-api.appspot.com/random_joke"))
+
+setup = ((output.split('setup":')[1]).split('","punchline"')[0])[1:]
+punchline = ((output.split('"punchline":')[1]))[1:-1]
+
+print('\n AI-PI: "',setup,'"')
+os.system('espeak "{}"'.format(setup)
+time.sleep(5)
+
+print('\n AI-PI: "',punchline,'"')
+os.system('espeak "{}"'.format(punchline))
