94a886e3a6b1eb30ddbac7e5e118cc37654e94c1
Shellywell123 <benjamin@shellswell.com>
2021-04-25 17:52:27 +0100

    added alarm.py

--- features/radio.py
+++ features/radio.py
@@ -1,10 +1,15 @@
 lofi = "https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=LofiGirl"
 
-import pafy
-import vlc
+#import pafy
+#import vlc
 
-video = pafy.new(lofi)
-best = video.getbest()
-playurl = best.lofi
+#video = pafy.new(lofi)
+#best = video.getbest()
+#playurl = best.lofi
 
-playurl.play()
+#playurl.play()
+
+EXPORT DISPLAY=0:0
+
+import os
+os.system('chromium-browser '+lofi)
