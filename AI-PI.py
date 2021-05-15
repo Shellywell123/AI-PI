5117242c197c28e5ff741cab8fc93eb5c600186c
Shellywell123 <benjamin@shellswell.com>
2021-05-15 21:16:39 +0100

    attempting alarm via threading

--- AI-PI.py
+++ AI-PI.py
@@ -1,4 +1,17 @@
-from features import date_time
-from features import weather
-from features import ip
-from features import stock
+# from features import date_time
+# from features import weather
+# from features import ip
+# from features import stock
+
+def thread_loop():
+    """
+    """
+    entry_every_N_mins = 1
+    minute = 60
+    threading.Timer(entry_every_N_mins*minute, thread_loop).start()
+
+    # check alarm every minute
+    from features.alarm import alarm_checker
+    alarm_checker()
+
+thread_loop()
--- features/alarm.py
+++ features/alarm.py
@@ -1,14 +1,17 @@
-import os 
+def alarm_check():
+    """
+    """
+    import os 
 
-day   = str(os.popen("date +%A").readlines())[2:-4]
-month_int = int(str(os.popen("date +%m").readlines())[2:-4])
-date  = str(os.popen("date +%W").readlines())[2:-4]
-year  = str(os.popen("date +%Y").readlines())[2:-4]
-time  = str(os.popen("date +%X").readlines())[2:-7]
+    day   = str(os.popen("date +%A").readlines())[2:-4]
+    month_int = int(str(os.popen("date +%m").readlines())[2:-4])
+    date  = str(os.popen("date +%W").readlines())[2:-4]
+    year  = str(os.popen("date +%Y").readlines())[2:-4]
+    time  = str(os.popen("date +%X").readlines())[2:-7]
 
-alarm_time = '17:50'
+    alarm_time = '17:50'
 
-print ('\n - AI-PI: "',time)
+    print ('\n - AI-PI: "'+time)
 
-if time==alarm_time:
-	os.system('espeak "wake up!"')
+    if time==alarm_time:
+    	os.system('espeak "wake up!"')
